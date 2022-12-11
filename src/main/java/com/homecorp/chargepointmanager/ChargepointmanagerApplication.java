9d6e126b3bdc405f8e13296897d60bb4523a5266
Andrey Zverev <andreizverev@yandex.ru>
2022-12-11 22:27:04 +0300

    - Add initial files from spring initializer

---
+++ src/main/java/com/homecorp/chargepointmanager/ChargepointmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.homecorp.chargepointmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargepointmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChargepointmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homecorp/chargepointmanager/ChargepointmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homecorp.chargepointmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargepointmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
